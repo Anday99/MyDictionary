a68380edf0c22d3dca9f0a73d0890949475d655c
ANDAY AKTAŞ <l5217039@std.yildiz.edu.tr>
2021-01-22 23:06:29 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<TKey,TValue>
+    {
+        TKey[] _keyArray;
+        TValue[] _valueArray;
+
+        public MyDictionary()
+        {
+            _keyArray = new TKey[0];
+            _valueArray = new TValue[0];
+        }
+
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] _tempKeyArray = _keyArray;
+            TValue[] _tempValueArray = _valueArray;
+
+            _keyArray = new TKey[_keyArray.Length+1];
+            _valueArray = new TValue[_valueArray.Length+1];
+
+            for (int i = 0; i < _tempKeyArray.Length ; i++)     //key ve value değerleri eşit olduğundan _tempKeyArray.Length veya _tempValueArray.Length kullanılabilir.
+            {
+                _keyArray[i] = _tempKeyArray[i];
+                _valueArray[i] = _tempValueArray[i];
+            }
+
+            _keyArray[_keyArray.Length-1] = key;
+            _valueArray[_valueArray.Length-1] = value;
+        }
+
+        public int CountKeys
+        {
+            get { return _keyArray.Length; }
+        }
+        public int CountValues
+        {
+            get { return _valueArray.Length; }
+        }
+
+        public TKey[] Keys
+        {
+            get { return _keyArray; }
+        }
+
+        public TValue[] Values
+        {
+            get { return _valueArray; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> nameAge = new MyDictionary<string, int>();
+            nameAge.Add("Anday", 21);
+            nameAge.Add("Begüm", 20);
+            nameAge.Add("Simay", 14);
+
+            Console.WriteLine("Dictionary içerisinde {0} key {1} value bulunmaktadır",nameAge.CountKeys,nameAge.CountValues);
+
+            Console.WriteLine("\n\nKey----->Value\n----------------");
+            for (int i = 0; i < nameAge.Keys.Length ; i++)
+            {
+                Console.WriteLine("{0}----->{1}\n",nameAge.Keys[i],nameAge.Values[i]);
+            }
+            Console.WriteLine("----------------\n");
+
+            nameAge.Add("Engin", 35);
+            nameAge.Add("Ozil", 32);
+
+            Console.WriteLine("Dictionary içerisinde {0} key {1} value bulunmaktadır", nameAge.CountKeys, nameAge.CountValues);
+
+            Console.WriteLine("\n\nKey----->Value\n----------------");
+            for (int i = 0; i < nameAge.Keys.Length; i++)
+            {
+                Console.WriteLine("{0}----->{1}\n", nameAge.Keys[i], nameAge.Values[i]);
+            }
+            Console.WriteLine("----------------\n");
+        }
+    }
+}
